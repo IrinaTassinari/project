70f2f6f29def772afbd0a2d3ee8e18cc4e5fb8a5
IrinaTassinari <irinatassinari@gmail.com>
2025-10-22 15:05:16 +0200

    addeed animation

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,13 @@
+new WOW().init();
+
+new WOW({
+  boxClass: 'wow',      // класс элементов для анимации
+  animateClass: 'animate__animated',
+  offset: 100,          // на сколько пикселей до появления элемента запускать анимацию
+  mobile: true,         // включить на мобильных
+  live: true            // обновлять при динамической загрузке контента
+}).init();
+
 const btnGreen = document.querySelector('.btn-green')
 btnGreen.addEventListener('click', () => {
     window.location.href = './index2.html'
